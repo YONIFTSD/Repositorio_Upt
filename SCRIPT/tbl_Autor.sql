5743a2535b42c11594d022854e3f054da790bb03
Juan Rodriguez <nyxjuan@hotmail.com>
2018-08-25 12:16:58 -0500

    Tablas Autor, Fichero

---
+++ SCRIPT/tbl_Autor.sql
@@ -0,0 +1,33 @@
+
+--REGISTROS DE LA TABLA AUTOR--
+
+INSERT INTO tbl_Autor VALUES('Juan Rodrigo' , 'Perez Gomez' , '1996-04-16',1,0,'2015-05-17');
+INSERT INTO tbl_Autor VALUES('rolando manuel' , 'Arias Pascal' , '1992-06-18',1,0,'2013-05-17');
+INSERT INTO tbl_Autor VALUES('Ignacio Jose' , 'Carpio Tejada' , '1994-05-30',1,0,'2012-05-17');
+INSERT INTO tbl_Autor VALUES('Rosa Carolina' , 'Gomez Peralta' , '1997-07-15',1,0,'2017-05-17');
+INSERT INTO tbl_Autor VALUES('Felipe Carlos' , 'Mamani Quispe' , '1995-08-15',1,0,'2016-05-17');
+INSERT INTO tbl_Autor VALUES('Martin Pedro' , 'Venabente Laura' , '1996-04-15',1,0,'2015-05-17');
+INSERT INTO tbl_Autor VALUES('Lucer Luz' , 'Arias Lopez' , '1992-06-18',1,0,'2013-05-17');
+INSERT INTO tbl_Autor VALUES('Jimena Carol' , 'Carpio Lozada' , '1994-04-16',1,0,'2012-05-17');
+INSERT INTO tbl_Autor VALUES('Estefany Carla' , 'Gomez Gomez' , '1997-04-11',1,0,'2017-05-17');
+INSERT INTO tbl_Autor VALUES('Manuel Diego' , 'Parcar Quispe' , '1995-04-12',1,0,'2016-05-17');
+INSERT INTO tbl_Autor VALUES('Fidel' , 'Gomel Ticona' , '1996-04-16',1,0,'2015-05-17');
+INSERT INTO tbl_Autor VALUES('Santos Tito' , 'Gomez Choquejahua' , '1992-06-18',1,0,'2013-05-17');
+INSERT INTO tbl_Autor VALUES('Gina Elizabeth' , 'Gotuzzo Ovalle' , '1994-04-16',1,0,'2012-05-17');
+INSERT INTO tbl_Autor VALUES('Oscar Eduardo' , 'Guerrero Vargas' , '1997-04-16',1,0,'2017-05-17');
+INSERT INTO tbl_Autor VALUES('Antonio' , 'Guillen Cama' , '1995-04-16',1,0,'2016-05-17');
+INSERT INTO tbl_Autor VALUES('Miguel Angel' , 'Guisa Bravo' , '1996-04-16',1,0,'2015-05-17');
+INSERT INTO tbl_Autor VALUES('Gabriela Isabel' , 'Heredia Álvarez' , '1992-06-18',1,0,'2013-05-17');
+INSERT INTO tbl_Autor VALUES('Avenilda Eufemia' , 'Herrera Chura' , '1994-04-16',1,0,'2012-05-17');
+INSERT INTO tbl_Autor VALUES('Cynthia Nohely' , 'Hinojosa Huaracallo' , '1997-04-16',1,0,'2017-05-17');
+INSERT INTO tbl_Autor VALUES('Amalia Juana' , 'Huaclla Gómez' , '1995-05-16',1,0,'2016-05-17');
+INSERT INTO tbl_Autor VALUES('Mauro Javier' , 'Hurtado Mejía' , '1996-05-16',1,0,'2015-05-17');
+INSERT INTO tbl_Autor VALUES('Ruth Mercedes' , 'Jines Cabezas' , '1992-06-18',1,0,'2013-05-17');
+INSERT INTO tbl_Autor VALUES('Jorge Josmell' , 'Larico Portugal' , '1994-08-16',1,0,'2012-05-17');
+INSERT INTO tbl_Autor VALUES('Richard' , 'Lazo Ramos' , '1997-04-16',1,0,'2017-05-17');
+INSERT INTO tbl_Autor VALUES('Richard Sabino' , 'Lazo Ramos' , '1995-04-16',1,0,'2016-05-17');
+INSERT INTO tbl_Autor VALUES('Fredy Reynaldo' , 'León Huayanca' , '1996-03-16',1,0,'2015-05-17');
+INSERT INTO tbl_Autor VALUES('Grace Cristina' , 'Levano Arredondo' , '1992-03-18',1,0,'2013-05-17');
+INSERT INTO tbl_Autor VALUES('Macarena Milagros' , 'Liendo Duarte' , '1994-01-16',1,0,'2012-05-17');
+INSERT INTO tbl_Autor VALUES('Elmer Marcial' , 'Limache Sandoval' , '1998-01-16',1,0,'2017-05-17');
+INSERT INTO tbl_Autor VALUES('Dora Gladys' , 'Mamani Flores' , '1998-01-16',1,0,'2016-05-17');
